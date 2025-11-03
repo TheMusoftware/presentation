5ba3ef7ba11855450d23931355f78a05dac73f9c
musoftware <mkazi581@gmail.com>
2025-11-03 21:35:43 +0300

    Added inf

--- wilkommen.py
+++ wilkommen.py
@@ -2,3 +2,7 @@
 print("Hello Earth!");
 print("Willkommen in der Welt der Programmierung!");
 print("asdas");
+print("Neue Zeile hinzugefügt.");
+print("Noch eine Zeile.");
+print("Ende des Programms.");
+print("Dies ist eine weitere Zeile.");
