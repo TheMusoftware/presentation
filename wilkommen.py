f0c891a1b1fba381eef14281e78b92a79ec3fd9a
alper <alper33yeni@gmail,com>
2025-11-03 21:37:04 +0300

    sdfgh

--- wilkommen.py
+++ wilkommen.py
@@ -4,5 +4,4 @@ print("Willkommen in der Welt der Programmierung!");
 print("asdas");
 print("Neue Zeile hinzugefügt.");
 print("Noch eine Zeile.");
-print("Ende des Programms.");
 print("Dies ist eine weitere Zeile.");
