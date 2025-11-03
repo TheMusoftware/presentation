5a60aebde51aba1dd50c8a705d7ecfae6dab8eaf
musoftware <mkazi581@gmail.com>
2025-11-03 21:38:56 +0300

    test

--- wilkommen.py
+++ wilkommen.py
@@ -1,7 +1,8 @@
 
-print("Hello asdfadsfghj");
+print("Hello alper!");
 print("Willkommen in der Welt der Programmierung!");
 print("asdas");
 print("Neue Zeile hinzugefügt.");
 print("Noch eine Zeile.");
+print("Ende des Programms.");
 print("Dies ist eine weitere Zeile.");
