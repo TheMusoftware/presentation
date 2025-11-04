814fd9faa85a7f9147f42665d173069ef3954bb4
alper <alper33yeni@gmail,com>
2025-11-04 13:09:10 +0300

    final

--- wilkommen.py
+++ wilkommen.py
@@ -1,10 +1,2 @@
 
-print("Hello Alper");
-print("Willkommen in der Welt der Programmierung!");
-print("dajsdlkaskda");
-print("Neue Zeile hinzugefügt.");
-print("Noch eine Zeile.");
-print("Dies ist eine weitere Zeile.");
-print("Noch eine Zeile.");
-print("Noch eine Zeile.");
-print("fkdsfds");
+print("Hello World");
